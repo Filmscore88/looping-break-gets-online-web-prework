a9806db241d2b1167cb5b218bc4d00d15b2cc098
Alberto Morales <cmtmorales24@gmail.com>
2019-02-23 08:03:25 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,8 +2,8 @@
 def levitation_quiz
 	loop do
 	  puts"What is the spell that enacts levitation?"
-	  name=car 
-	 break if name=="Wingardium Leviosa"
+	  name=get.chomp
+	 break "Wingardium Leviosa"
 	 end
 	 puts "You passed the quiz!"
 end
